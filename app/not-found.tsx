894350fae6d7631acd8bc6f9ddd47140089cfa83
Olesia Konashchuk <konaschukl@gmail.com>
2025-12-29 01:50:17 +0100

    metadata correction

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,23 @@
-
+import type { Metadata } from "next";
 import css from "./page.module.css";
+
+export const metadata: Metadata = {
+  title: "404 - Page not found | NoteHub",
+  description: "The page you are looking for does not exist.",
+  openGraph: {
+    title: "404 - Page not found | NoteHub",
+    description: "The page you are looking for does not exist.",
+    url: "http://localhost:3000//not-found",
+    images: [
+      {
+        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+        width: 1200,
+        height: 630,
+        alt: "404 - Page not found",
+      },
+    ],
+  },
+};
 
 const NotFound = () => {
   return (
--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,4 +1,5 @@
 import { fetchNotes } from '@/lib/api';
+import { Metadata } from 'next';
 import NotesClient from './Notes.client';
 
 import { QueryClient, HydrationBoundary, dehydrate } from '@tanstack/react-query';
@@ -6,6 +7,29 @@ import { QueryClient, HydrationBoundary, dehydrate } from '@tanstack/react-query
 type Props = {
   params: Promise<{ slug: string[] }>;
 };
+
+export async function generateMetadata({params}: Props): Promise<Metadata> {
+  const filter = (await params).slug[0];
+  const title = filter === "all" ? "All notes | NoteHub" : `Notes: ${filter} | NoteHub`;
+  const description = filter === "all" ? "Browse all notes" : `Browse notes filtered: ${filter}`;
+
+  return{
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      url: `http://localhost:3000//notes/filter/${filter}`,
+      images:[{
+        url: "",
+        width: 1200,
+        height: 630,
+        alt: "Notes page"
+      }]
+    }
+  }
+}
+
 
 export default async function Notes({ params }: Props) {
   const queryClient = new QueryClient();
